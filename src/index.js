b05059f8014bfe2cfb5c575d60cf5f73110fbc08
Joshua Sevy <joshuasevy@outlook.com>
2020-09-25 16:15:44 -0600

    Add reducer and action functino to index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import './index.css';
 
+export const addToDo = todo => ({
+  type: 'ADD_TODO',
+  todo
+});
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
