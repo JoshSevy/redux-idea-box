9c4f27ef58632e2dbdf30e28b6392457b2f025e9
Joshua Sevy <joshuasevy@outlook.com>
2020-09-25 16:24:44 -0600

    Add todo reducer

--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import './index.css';
 
-export const addToDo = todo => ({
-  type: 'ADD_TODO',
-  todo
-});
+
 
 ReactDOM.render(
   <App />,
